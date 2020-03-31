cd47a52627d64f68bc0b6c1fd0dffd71909d11ea
William Niemiec <williamniemiec@hotmail.com>
2020-03-31 19:23:02 -0300

    JUnit example

---
+++ tests/executionFlow/runtime/JUnitSimulation.java
@@ -0,0 +1,24 @@
+package executionFlow.runtime;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+/**
+ * Class created to show the application working with
+ * JUnit test case in real time.
+ * 
+ * @apiNote RuntimeCollector.aj has to be in the same package 
+ */
+public class JUnitSimulation 
+{
+	@Test
+	public void testFactorial() {
+		int num = 4;
+		long expectedResult = 24; 
+		TestClass tc = new TestClass(4);
+		long res = tc.factorial(num);
+		
+		assertEquals(expectedResult, res);
+	}
+}
