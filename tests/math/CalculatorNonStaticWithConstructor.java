2b6d5abf8d383ab89969c4eaebb2c7f6b77440f1
William Niemiec <williamniemiec@hotmail.com>
2020-04-03 11:44:43 -0300

    Math package fix

---
+++ tests/math/CalculatorNonStaticWithConstructor.java
@@ -0,0 +1,33 @@
+package math;
+
+
+public class CalculatorNonStaticWithConstructor 
+{
+	private int x;
+	
+	
+	public CalculatorNonStaticWithConstructor(int x) {
+		this.x = x;
+	}
+	
+	
+	public int sum(int a, int b) 
+	{
+		var sum = a;
+		
+		for (var x = 0; x < b; x++) {
+			sum++;
+		}
+		
+		return sum;
+	}
+	
+	public void loop() 
+	{
+		int k = 0;
+		
+		while (k < x) {
+			k++;
+		}
+	}
+}
