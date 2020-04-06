4932b63376894ab397b85a253afa96ea5931f72d
William Niemiec <williamniemiec@hotmail.com>
2020-04-06 15:14:31 -0300

    Console exporter fix

---
+++ tests/executionFlow/runtime/MultipleTestPaths.java
@@ -0,0 +1,17 @@
+package executionFlow.runtime;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class MultipleTestPaths 
+{
+	@Test
+	public void ThreeTestPathsTest() 
+	{
+		TestClass tc = new TestClass(99);
+		assertEquals("one", tc.threePaths(1));
+		assertEquals("two", tc.threePaths(2));
+		assertEquals("", tc.threePaths(3));
+	}
+}
