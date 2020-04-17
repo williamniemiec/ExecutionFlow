d53d9e4440d6dcc58fe0740d9ea7e4c7cb41ecff
William Niemiec <williamniemiec@hotmail.com>
2020-04-17 10:30:44 -0300

    Code refactoring

--- src/executionFlow/runtime/CollectorExecutionFlow.java
+++ src/executionFlow/runtime/CollectorExecutionFlow.java
@@ -25,6 +25,9 @@ public class CollectorExecutionFlow
 	//-----------------------------------------------------------------------
 	//		Attributes
 	//-----------------------------------------------------------------------
+	/**
+	 * Necessary for findCurrentClassPath method;
+	 */
 	private static String classPath;
 
 	
