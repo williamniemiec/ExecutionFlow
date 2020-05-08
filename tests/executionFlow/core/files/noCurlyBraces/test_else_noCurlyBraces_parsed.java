f047503dff0272fa3607d440bb3b8618cc499552
William Niemiec <williamniemiec@hotmail.com>
2020-05-08 14:02:08 -0300

    FileParser noCurlyBrackets

---
+++ tests/executionFlow/core/files/noCurlyBraces/test_else_noCurlyBraces_parsed.java
@@ -0,0 +1,28 @@
+public class test_else_noCurlyBraces 
+{
+	public String ifElseMethod(int num)
+	{
+		if (num < 0)
+			return "";
+		
+		String response = "";
+		int w;int _b100f06c277acb9f2130e312af63bd1c=7;
+		w = 0;
+
+		if (num == 0)
+			response = "Number zero";
+		else if (num == 1)
+			response = "Number one";
+		else {_b100f06c277acb9f2130e312af63bd1c=7;
+			if (false)
+				w++;
+			else if (false) w++;
+			else {_b100f06c277acb9f2130e312af63bd1c=7;
+				for(;;)
+					for(;;)
+						w++;}
+		}
+		
+		return response;
+	}
+}
