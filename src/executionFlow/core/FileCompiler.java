e5a8c96c283de76a8190f3fc3aec0d81ebc0aed9
William Niemiec <williamniemiec@hotmail.com>
2020-05-10 13:04:57 -0300

    FileCompiler doc

--- src/executionFlow/core/FileCompiler.java
+++ src/executionFlow/core/FileCompiler.java
@@ -8,45 +8,31 @@ import java.util.List;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 
+
+/**
+ * Responsible for compiling .java files.
+ */
 public class FileCompiler 
 {
-	private File file;
-	
-	public FileCompiler(String filepath)
-	{
-		System.out.println("FP: "+filepath);
-		//this.file = new File(filepath);
-	}
-	
+	//-------------------------------------------------------------------------
+	//		Methods
+	//-------------------------------------------------------------------------
 	/**
+	 * Compiles .java file.
 	 * 
 	 * @param fileToCompile Absolute path of source file to be compiled
-	 * @param outputDir
-	 * @return
-	 * @throws Exception 
+	 * @param outputDir Path where generated .class will be saved
+	 * @return Path of generated .class
+	 * @throws Exception If an error occurs during compilation
 	 */
 	public static String compile(File fileToCompile, String outputDir) throws Exception
 	{
-//		System.out.println("output: "+outputDir);
-		//String fileToCompile = "test_else.java";
-		//String[] args = new String[3];
-		/*
-		List<String> args = new ArrayList<>();
-		if (outputDir != null) {
-			args.add("-d");
-			args.add("bin");
-		}
-		
-		args.add(fileToCompile);
-		*/
-		
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		int compilationResult =	compiler.run(null, null, null, new String[] {"-d", outputDir, fileToCompile.getAbsolutePath()});
+		
 		if(compilationResult != 0) {
 			throw new Exception("Compilation Failed");
 		}
-		//System.out.println("FILE TO COMPILE: "+fileToCompile);
-//		System.out.println("COMPILED "+outputDir+"\\"+fileToCompile.getName().split("\\.")[0]+".class");
 		
 		return outputDir+"\\"+fileToCompile.getName().split("\\.")[0]+".class";
 	}
