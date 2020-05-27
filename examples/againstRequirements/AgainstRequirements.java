07cefcace6cf743a034aa7b4c66bc1eff294840d
William Niemiec <williamniemiec@hotmail.com>
2020-05-27 09:28:09 -0300

    AgainstRequirements - testLastCurlyBracketInSameLine

--- examples/againstRequirements/AgainstRequirements.java
+++ examples/againstRequirements/AgainstRequirements.java
@@ -11,9 +11,19 @@ import org.junit.Test;
 public class AgainstRequirements 
 {
 	/**
-	 * Test with curly bracket in the same line of last instruction.
+	 * Test with curly bracket in the same line of last instruction. No test 
+	 * path will be computed.
 	 */
-	
+	@Test
+	public void testLastCurlyBracketInSameLine()
+	{
+		System.out.println("#####################################################################");
+		System.out.println("                    testLastCurlyBracketInSameLine                   ");
+		System.out.println("#####################################################################");
+		
+		AgainstRequirementsTestClass artc = new AgainstRequirementsTestClass();
+		
+		assertEquals(24, artc.factorial(4));	}
 	
 	/**
 	 * Test without {@link Test} annotation.
