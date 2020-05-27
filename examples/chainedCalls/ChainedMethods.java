c64f224e0c9718c931a75dca4c7ae12e6e941f01
William Niemiec <williamniemiec@hotmail.com>
2020-05-27 09:39:26 -0300

    ChainedMethods - testChainedMethodsInForLoop

--- examples/chainedCalls/ChainedMethods.java
+++ examples/chainedCalls/ChainedMethods.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 public class ChainedMethods 
 {
-	/*
 	@Test
 	public void testChainedMethods()
 	{
@@ -14,7 +13,7 @@ public class ChainedMethods
 		
 		// ( ( (15 + 15 + 15 - 15) * 2) / 10) / 2
 		assertEquals(3f, c.setNumber(15).sum(15).sum(15).sub(15).mult(2).div(10).div(2).ans(), 0.01);
-	}*/
+	}
 	
 	@Test
 	public void testChainedMethodsInForLoop()
