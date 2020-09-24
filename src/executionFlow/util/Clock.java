217a112fb537bc2fe604ca236efb9cf9ed7c601a
William Niemiec <williamniemiec@hotmail.com>
2020-09-24 20:47:33 -0300

    Clock - clearTimeout method

--- src/executionFlow/util/Clock.java
+++ src/executionFlow/util/Clock.java
@@ -14,44 +14,76 @@ import java.util.TimerTask;
  */
 public class Clock 
 {
+	//-------------------------------------------------------------------------
+	//		Attributes
+	//-------------------------------------------------------------------------
 	/**
-	 * Stores routines sent to the setInterval method along with its ids.
+	 * Stores routines sent to setInterval method along with its ids.
 	 */
 	private static Map<Integer, Timer> intervalRoutines = new HashMap<>();
 	
+	/**
+	 * Stores routines sent to setTimeout method along with its ids.
+	 */
+	private static Map<Integer, Timer> timeoutRoutines = new HashMap<>();
 	
+	
+	//-------------------------------------------------------------------------
+	//		Methods
+	//-------------------------------------------------------------------------
 	/**
 	 * Sets a timer which executes a routine once the timer expires.
 	 * 
 	 * @param		routine Routine to be performed
+	 * @param		id Routine identifier (necessary to be able to stop it)
 	 * @param		delay Waiting time before the routine is executed (in 
 	 * milliseconds)
+	 * 
+	 * @return		False if an timeout has has already been set with the 
+	 * given id or true otherwise
 	 */
-	public static void setTimeout(Runnable routine, int delay)
+	public static boolean setTimeout(Runnable routine, int id, int delay)
 	{
-		new Thread(() -> {
-	        try {
-	            Thread.sleep(delay);
-	            routine.run();
-	        }
-	        catch (Exception e) {
-	            System.err.println(e);
-	        }
-	    }).start();
+		if (timeoutRoutines.containsKey(id))
+			return false;
+		
+		Timer t = new Timer();
+		
+		
+		timeoutRoutines.put(id, t);
+		t.schedule(new TimerTask(){
+		    @Override
+		    public void run()
+		    {
+		       routine.run();
+		    }
+		}, delay);
+		
+		return true;
+		
+//		new Thread(() -> {
+//	        try {
+//	            Thread.sleep(delay);
+//	            routine.run();
+//	        }
+//	        catch (Exception e) {
+//	            System.err.println(e);
+//	        }
+//	    }).start();
 	}
 	
 	/**
 	 * Repeatedly calls a routine with a fixed time delay between each call.
 	 * 
-	 * @param		id Routine identifier (necessary to be able to stop it)
 	 * @param		routine Routine to be performed
+	 * @param		id Routine identifier (necessary to be able to stop it)
 	 * @param		interval Interval that the routine will be invoked (in 
 	 * milliseconds)
 	 * 
 	 * @return		False if an interval has has already been set with the 
 	 * given id or true otherwise
 	 */
-	public static boolean setInterval(int id, Runnable routine, int interval)
+	public static boolean setInterval(Runnable routine, int id, int interval)
 	{
 		if (intervalRoutines.containsKey(id))
 			return false;
@@ -82,6 +114,20 @@ public class Clock
 		if (intervalRoutines.containsKey(id)) {
 			intervalRoutines.get(id).cancel();
 			intervalRoutines.remove(id);
+		}
+	}
+	
+	/**
+	 * Cancels a timed action which was previously established by a
+	 * call to {@link #setTimeout(int, Runnable, int)}.
+	 * 
+	 * @param		id Routine id
+	 */
+	public static void clearTimeout(int id)
+	{
+		if (timeoutRoutines.containsKey(id)) {
+			timeoutRoutines.get(id).cancel();
+			timeoutRoutines.remove(id);
 		}
 	}
 }
