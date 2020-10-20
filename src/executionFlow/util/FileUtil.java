55ddc77f3f706eb5172e0e9598f1152c1a738217
William Niemiec <williamniemiec@hotmail.com>
2020-10-20 20:53:17 -0300

    createArgumentFile

--- src/executionFlow/util/FileUtil.java
+++ src/executionFlow/util/FileUtil.java
@@ -2,6 +2,7 @@ package executionFlow.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -81,5 +82,49 @@ public class FileUtil
 			if (Files.notExists(target))
 				Files.copy(p, target);
 		}
+	}
+	
+	/**
+	 * Generates argument file from a list of paths.
+	 * 
+	 * @param		dir Directory where the file will be stored
+	 * @param		filename Argument file name
+	 * @param		paths List of paths
+	 * 
+	 * @return		Argument file
+	 * 
+	 * @throws 		IOException If an error occurs while writing the file
+	 * 
+	 * @see			https://docs.oracle.com/javase/9/tools/java.htm#JSWOR-GUID-4856361B-8BFD-4964-AE84-121F5F6CF111
+	 */
+	public static Path createArgumentFile(Path dir, String filename, List<Path> paths) throws IOException
+	{
+		Path argumentFile = dir.resolve(filename);
+		
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(argumentFile.toFile()))) {
+			Path path;
+			int totalPaths = paths.size();
+			
+			// Writes header
+			bw.write("\"\\");
+			bw.newLine();
+			
+			// Writes list of paths
+			for (int i=0; i<totalPaths-1; i++) {
+				path = paths.get(i);
+				
+				bw.write(path.toAbsolutePath().toString());
+				bw.write(";");
+				bw.newLine();
+			}
+			
+			// Writes last item + footer
+			path = paths.get(totalPaths-1);
+			bw.write(path.toAbsolutePath().toString());
+			bw.write("\"");
+		}
+		
+		return argumentFile;
 	}
 }
